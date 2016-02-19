7f9af3a264c3b001fa4c15e916341d40b77077ac
vjarysta <vjarysta@gmail.com>
2016-02-19 09:27:53 +0000

    Logo integration + Deleted header menu useless elements + All routes lead to /projects

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -6,17 +6,17 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 
     // Redirect to 404 when route not found
     $urlRouterProvider.otherwise(function ($injector, $location) {
-      $injector.get('$state').transitionTo('not-found', null, {
+      $injector.get('$state').transitionTo('projects-list', null, {
         location: false
       });
     });
 
     // Home state routing
     $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'modules/core/client/views/home.client.view.html'
-      })
+      // .state('home', {
+      //   url: '/',
+      //   templateUrl: 'modules/core/client/views/home.client.view.html'
+      // })
       .state('not-found', {
         url: '/not-found',
         templateUrl: 'modules/core/client/views/404.client.view.html',
